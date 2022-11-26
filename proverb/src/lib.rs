c66ee4cf12f90e7b8e82d5cccbe99761221d65bb
Jake Klingler <thearrow3456@gmail.com>
2022-11-26 10:10:21 -0500

    proverb

---
+++ proverb/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    if list.is_empty() {
+        return String::new();
+    }
+
+    let mut sentences = list
+        .windows(2)
+        .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
+        .collect::<Vec<String>>();
+    sentences.push(format!("And all for the want of a {}.", list[0]));
+    sentences.join("\n")
+}
